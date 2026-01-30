c0f1d9014afa8faf26373c5a74af7f93d0146f48
Aku <al0832@edu.turku.fi>
2026-01-30 13:23:43 +0200

    added UI

---
+++ main.py
@@ -0,0 +1,13 @@
+from colorama import Fore
+import ddoser
+import doxxxxxxxx
+print(Fore.RED+ "IP" + Fore.GREEN + "-DDOXXR v1.0")
+print(Fore.BLUE)
+a = input("Do you want to use DDOSER (1) or DOXXXER (2)? ")
+match(a):
+    case "1":
+        ddoser.ddoser()
+    case "2":
+        doxxxxxxxx.doxxer()
+    case default:
+        print("invalid tool")
